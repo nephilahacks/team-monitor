ed4cab73c6af1ea774217fe2e123a0885d4736ae
Andrea Stagi <stagi.andrea@gmail.com>
2015-06-08 08:25:06 +0200

    Serve partials from backend

--- teamonitor/app.js
+++ teamonitor/app.js
@@ -39,6 +39,11 @@ app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/partials/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
+});
+
 app.use(apiEndpoint + '/sprints', sprints);
 
 // catch 404 and forward to error handler
